3c9babfaa4c52aa75b18a694c684914112e17398
sassy-xx <103501164+sassy-xx@users.noreply.github.com>
2023-03-25 18:08:20 +0000

    Create return_keys.php
    Endpoint to return api keys, and the data relating to said api keys.

---
+++ api/api_editor/return_keys.php
@@ -0,0 +1,56 @@
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'].'/index.php');
+
+    class return_keys {
+        public bool $success;
+        public mixed $data;
+        public mixed $error;
+        public int $http_code;
+        public function __construct() {
+            // initialise the endpoint
+            $init = self::init();
+            if(isset($init['success']) && isset($init['data']) && isset($init['error'])) {
+                $this->success = $init['success'];
+                $this->data = $init['data'];
+                $this->error = $init['error'];
+            } else {
+                $this->success = false;
+                $this->data = null;
+                $this->error = 'Bad endpoint structure. Missing basic array return.';
+            }
+        }
+        
+        private static function init() {
+            // look up all of the current endpoints and return them in a sesnible format
+            $sql = "SELECT
+                        *
+                    FROM
+                        api.api_keys k";
+            $mysql = new mysql_(DEFAULT_DB_CONN);
+            $mysql->sql_select($sql);
+            if($mysql->result->num_rows < 1) {
+                return [
+                    'success' => false,
+                    'data' => null,
+                    'error' => [
+                        'message' => SELECT_DB_ERROR
+                    ]
+                ];
+            }
+            $return_array = [];
+            foreach($mysql->result as $k => $v) {
+                foreach($v as $kk => $vv) {
+                    if(!isset($return_array[$k][$kk])) {
+                        $return_array[$k][$kk] = [];
+                    }
+                    $return_array[$k][$kk] = $vv;
+                }
+            }
+            return [
+                'success' => true,
+                'data' => $return_array,
+                'error' => false
+            ];
+        }
+    }
+?>
